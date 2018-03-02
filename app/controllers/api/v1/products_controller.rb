421e0ae6e0ebbe40590a84ca048e21bfd748d30c
jairorjuela <genius85862@gmail.com>
2018-03-02 16:24:28 +0100

    JSON

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,11 @@
+module Api
+    module V1
+        class ProductsController < ApplicationController
+          def index
+            @products = Product.all
+
+            render json: @products 
+          end
+        end
+    end
+end

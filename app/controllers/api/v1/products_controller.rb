17d411e7e400e51891b5378db4a2d5c1f5dd2c8c
jairorjuela <genius85862@gmail.com>
2018-03-18 20:22:23 -0500

    create_products_1

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,13 +1,30 @@
 module Api
     module V1
         class ProductsController < ApplicationController
+          skip_before_action :verify_authenticity_token
+          
           def index
             @products = Product.all
             render json: @products
           end
 
+          def new
+            @product = Product.new
+          end
+
           def create
-            
+            @product = Product.new(product_params)
+            if @product.save
+              render json: @products, status: 201
+            else
+              render json: {errors: product.errors }, status: 422
+            end
+          end
+
+          private
+          def product_params
+            params.require(:product).permit(:name, :price)
+          end
         end
     end
 end

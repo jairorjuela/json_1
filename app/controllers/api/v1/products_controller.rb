adfe7e28ae31376c4f5687949c8bddcb2c05d041
jairorjuela <genius85862@gmail.com>
2018-03-18 20:26:47 -0500

    create_products_2

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,7 +2,7 @@ module Api
     module V1
         class ProductsController < ApplicationController
           skip_before_action :verify_authenticity_token
-          
+
           def index
             @products = Product.all
             render json: @products
@@ -17,7 +17,7 @@ module Api
             if @product.save
               render json: @products, status: 201
             else
-              render json: {errors: product.errors }, status: 422
+              render json: {errors: @product.errors }, status: 422
             end
           end
 

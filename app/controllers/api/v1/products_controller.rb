dc2fc9fa3cc0f44a23b616e153f0629396590fae
jairorjuela <genius85862@gmail.com>
2018-03-18 23:47:58 -0500

    delete_products_1

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -21,6 +21,22 @@ module Api
             end
           end
 
+          def update
+            @product = Product.find(params[:id])
+
+            if @product.update(product_params)
+              render json: @products, status: 200
+            else
+              render json: {errors: product.errors }, status: 422
+            end
+          end
+
+          def destroy
+            @product = Product.find(params[:id])
+            @product.destroy
+            head 204
+          end
+
           private
           def product_params
             params.require(:product).permit(:name, :price)
